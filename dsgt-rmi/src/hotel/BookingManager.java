0569b722fdc45fd2adcfe582ec7334d54928000c
Olgerard <olivier.gerard.2004@hotmail.com>
2026-02-13 20:01:41 +0100

    Full exercise

---
+++ dsgt-rmi/src/hotel/BookingManager.java
@@ -0,0 +1,71 @@
+package hotel;
+
+import java.time.LocalDate;
+import java.util.*;
+
+public class BookingManager {
+
+	private Room[] rooms;
+
+	public BookingManager() {
+		this.rooms = initializeRooms();
+	}
+
+	public Set<Integer> getAllRooms() {
+		Set<Integer> allRooms = new HashSet<Integer>();
+		Iterable<Room> roomIterator = Arrays.asList(rooms);
+		for (Room room : roomIterator) {
+			allRooms.add(room.getRoomNumber());
+		}
+		return allRooms;
+	}
+
+	public boolean isRoomAvailable(Integer roomNumber, LocalDate date) {
+		for (Room room : rooms){
+			if (room.getRoomNumber().equals(roomNumber)){
+				for(BookingDetail bookingDetail :  room.getBookings()){
+					if (bookingDetail.getDate().equals(date)){
+						return false;
+					}
+				}
+			}
+		}
+		return true;
+	}
+
+	public void addBooking(BookingDetail bookingDetail) {
+		int roomNumber = bookingDetail.getRoomNumber();
+		for  (Room room : rooms){
+			if  (room.getRoomNumber().equals(roomNumber)){
+				room.getBookings().add(bookingDetail);
+				return;
+			}
+		}
+	}
+
+	public Set<Integer> getAvailableRooms(LocalDate date) {
+		Set<Integer> free_rooms = new HashSet<>();
+		for (Room room : rooms){
+			boolean free = true;
+			for(BookingDetail bookingDetail :  room.getBookings()){
+                if (bookingDetail.getDate().equals(date)) {
+                    free = false;
+                    break;
+                }
+			}
+			if (free){
+				free_rooms.add(room.getRoomNumber());
+			}
+		}
+		return free_rooms;
+	}
+
+	private static Room[] initializeRooms() {
+		Room[] rooms = new Room[4];
+		rooms[0] = new Room(101);
+		rooms[1] = new Room(102);
+		rooms[2] = new Room(201);
+		rooms[3] = new Room(203);
+		return rooms;
+	}
+}
---
+++ dsgt-rmi/src/staff/BookingClient.java
@@ -0,0 +1,78 @@
+package staff;
+
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.time.LocalDate;
+import java.util.Set;
+
+import hotel.BookingDetail;
+import hotel.BookingManager;
+import hotel.HotelInterface;
+
+public class BookingClient extends AbstractScriptedSimpleTest {
+
+	private HotelInterface bm = null;
+
+	public static void main(String[] args) throws Exception {
+		BookingClient client = new BookingClient();
+		client.run();
+	}
+
+	/***************
+	 * CONSTRUCTOR *
+	 ***************/
+	public BookingClient() {
+		try {
+			Registry registry = LocateRegistry.getRegistry(null);
+			HotelInterface stub = (HotelInterface) registry.lookup("HotelServer");
+			bm = stub;
+			System.out.println("Server found by client");
+		} catch (Exception e) {
+			System.err.println("Client exception: " + e.toString());
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public boolean isRoomAvailable(Integer roomNumber, LocalDate date) {
+		try {
+			return bm.isRoomAvailable(roomNumber, date);
+		} catch (Exception e) {
+			System.err.println("Client method (isRoomAvailable) exception: " + e.toString());
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	@Override
+	public void addBooking(BookingDetail bookingDetail) throws Exception {
+		try {
+			bm.addBooking(bookingDetail);
+		} catch (Exception e) {
+			System.err.println("Client method (addBooking) exception: " + e.toString());
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public Set<Integer> getAvailableRooms(LocalDate date) {
+		try {
+			return bm.getAvailableRooms(date);
+		} catch (Exception e) {
+			System.err.println("Client method (getAvailableRooms) exception: " + e.toString());
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	@Override
+	public Set<Integer> getAllRooms() {
+		try {
+			return bm.getAllRooms();
+		} catch (Exception e) {
+			System.err.println("Client method (getAllRooms) exception: " + e.toString());
+			e.printStackTrace();
+			return null;
+		}
+	}
+}
